3b1424f67a31cf33118ab823d30917c7f633f64e
JuliusGrote <julius.grote@gmx.de>
2024-05-12 16:59:30 +0200

    Branch Updates: added option to only use pretrials and adjusted all files accrodingly.  - small spelling mistakes and language errors fixed as well.
    Signed-off-by: JuliusGrote <julius.grote@gmx.de>

--- instructions.py
+++ instructions.py
@@ -5,6 +5,10 @@ class Instructions:
 	## Check what is given in the config files and set instuctions accordingly.
 	## naming is so that a "font" is used in a text stimulus in PsychoPy while stimuli that are used as images are "img" here
 	def set_instructions(self, language, Targeted_duration, N_repeats):
+		#since "font_explain_trials's" f-string uses interger calculation, if "main_trials" are disabled in the config N_repeats must be "0" instead of "None"...
+		if N_repeats == None:
+			N_repeats  = 0
+			
 		if language == 'German':
 			self.font_check_response = 'Bitte pressen Sie jede Taste der Responsebox zwei mal!'
 			self.font_check_response_2 = 'Responsebox erfolgreich gecheckt!'
@@ -26,7 +30,7 @@ class Instructions:
 			self.font_motor = 'Hand: Drücken Sie die Tasten ABWECHSELND zum Rhythmus, der auf dem Bildschirm zu sehen ist!'
 			self.font_watch = 'Auge: Schauen Sie Tetris zu! Schauen Sie einfach nur zu, während eine Spiel-Aufzeichnung für Sie gespielt wird. Bitte Drücken Sie KEINE Tasten!'
 			self.font_cross = 'Fixationskreuz: Schauen Sie einfach das Kreuz in der Bildschirmmitte an und machen Sie nichts Anderes!'
-			self.font_explain_trials = f'Jede Wiederholung startet mit einem Teil TETRIS SPIELEN, gefolgt von einem zufällig gewählten Teil TETRIS ZUZUSCHAUEN, TASTENDRÜCKEN oder FIXATIONSKREUZ! Jeder Teil dauert {Targeted_duration} Sekunden und es gibt {N_repeats * 3} Wiederholungen!'
+			self.font_explain_trials = f'Jede Wiederholung startet mit einem Teil TETRIS SPIELEN, gefolgt von einem zufällig gewählten Teil TETRIS ZUZUSCHAUEN, TASTENDRÜCKEN oder FIXATIONSKREUZ! Jeder Teil dauert {Targeted_duration} Sekunden und es gibt {int(N_repeats) * 3} Wiederholungen!'
 			self.font_start = 'Jetzt beginnt das Experiment!'
 			self.font_condition_ended = 'Teil beendet'
 			self.font_end = 'Vielen Dank für Ihre Teilnahme!'
@@ -51,14 +55,8 @@ class Instructions:
 			self.font_explain_comp_speed = 'Note! In each Tetris part, it is randomly chosen, whether you will play in a HIGHER (faster) or LOWER (slower) level!'
 			self.font_motor = 'Hand: Press the Buttons ALTERNATELY (one after another) to the rhythm displayed on the screen!'
 			self.font_watch = 'Eye: Watch Tetris! Just watch while a game recording is played for you. Please DO NOT press any buttons!'
-			self.font_cross = 'Fixation Cross: Just look at the cross in the middle of the screen and do nothing else!'
+			self.font_cross = 'Fixation Cross: Just look at the cross in the middle of the screen and do nothing else!'		
 			self.font_explain_trials = f'Each repetition starts with a round of PLAYING TETRIS followed by a randomly chosen part of WATCHING TETRIS GAMEPLAY, BUTTON PRESSES or FIXATION CROSS! Each part lasts {Targeted_duration} seconds and there are {N_repeats * 3} repetitions!'
 			self.font_start = 'Now, the experiment starts!'
 			self.font_condition_ended = 'Part ended'
 			self.font_end = 'Thank you for your Participation!'
-			
-			
-			
-			
-		
-		
